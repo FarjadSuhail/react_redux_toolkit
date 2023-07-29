f96d7a9e22cf57a53388fd57ebb5a3440a380fda
Farjad Suhail <farjad.suhail@systemsltd.com>
2023-07-29 20:29:25 +0500

    redux/toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,48 +1,51 @@
 import { createStore } from "redux";
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import counterSlice from "./slice/counter-slice";
+import authSlice from "./slice/auth-slice";
+// // // // import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialCounterState = { counter: 0, showCounter: true };
+// // // // const initialCounterState = { counter: 0, showCounter: true };
 
-// prepare a slice of our global state
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-        increment(state) {
-            //imer package
-            state.counter++;
-        },
-        decrement(state) {
-            state.counter--;
-        },
-        increase(state, action) {
-            state.counter = state.counter + action.payload;
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter;
-        },
-    }
-});
+// // // // // prepare a slice of our global state
+// // // // const counterSlice = createSlice({
+// // // //     name: 'counter',
+// // // //     initialState: initialCounterState,
+// // // //     reducers: {
+// // // //         increment(state) {
+// // // //             //imer package
+// // // //             state.counter++;
+// // // //         },
+// // // //         decrement(state) {
+// // // //             state.counter--;
+// // // //         },
+// // // //         increase(state, action) {
+// // // //             state.counter = state.counter + action.payload;
+// // // //         },
+// // // //         toggleCounter(state) {
+// // // //             state.showCounter = !state.showCounter;
+// // // //         },
+// // // //     }
+// // // // });
 
-const initialAuthState = {
-    isAuthenticated: false
-}
-console.log(initialAuthState);
+// // // // const initialAuthState = {
+// // // //     isAuthenticated: false
+// // // // }
+// // // // console.log(initialAuthState);
 
-const authSlice = createSlice({
-    name: 'authentication',
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            console.log(state);
-            console.log("aaaaaaaaaaaaaaaaa");
-            state.isAuthenticated = true;
-        },
-        logout(state) {
-            state.isAuthenticated = false;
-        }
-    }
-});
+// // // // const authSlice = createSlice({
+// // // //     name: 'authentication',
+// // // //     initialState: initialAuthState,
+// // // //     reducers: {
+// // // //         login(state) {
+// // // //             console.log(state);
+// // // //             console.log("aaaaaaaaaaaaaaaaa");
+// // // //             state.isAuthenticated = true;
+// // // //         },
+// // // //         logout(state) {
+// // // //             state.isAuthenticated = false;
+// // // //         }
+// // // //     }
+// // // // });
 
 // if just 1 createSlice then use below method 
 //const store = createStore(counterSlice.reducer); 
@@ -62,6 +65,6 @@ const store = configureStore({
     // {
     //     type: 'some auto generated unique identifer'
     // }
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
+// export const counterActions = counterSlice.actions;
+// export const authActions = authSlice.actions;
 export default store;
